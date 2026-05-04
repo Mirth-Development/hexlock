47308d37f237c02bfb25f7981067275de380b436
Gabriel Allison <gabelot12@gmail.com>
2026-05-03 21:21:26 -0400

    Added Spring squish function
    added function that squishes the spring sprite between the top of the chamber and the tumbler.

--- lockpick/src/features/lock/spring/components.rs
+++ lockpick/src/features/lock/spring/components.rs
@@ -2,4 +2,14 @@ use bevy::prelude::Component;
 
 //Naming Component
 #[derive(Component)]
-pub struct SpringComponent;
+pub struct SpringComponent{
+    pub position: u32
+}
+
+impl Default for SpringComponent{
+    fn default() -> Self {
+        Self {
+            position: 0
+        }
+    }
+}
--- lockpick/src/features/lock/spring/systems.rs
+++ lockpick/src/features/lock/spring/systems.rs
@@ -1,1 +1,24 @@
 use bevy::prelude::*;
+use crate::features::lock::resource::TumblerSpringPairings;
+use crate::features::lock::spring::components::SpringComponent;
+use crate::features::lock::tumblers::components::{TumblerComponent};
+use crate::features::lock::tumblers::systems::HEIGHT_OF_TUMBLER_SPRITE;
+use crate::features::lock::systems::TOP_OF_CHAMBER;
+
+pub const HEIGHT_OF_SPRING_SPRITE: f32= 92.0;
+pub fn stretch_to_tumbler(
+    tumbler_spring_pairings: Res<TumblerSpringPairings>,
+    transforms: Query<&Transform, Without<SpringComponent>>,
+    mut spring_transforms: Query<&mut Transform, With<SpringComponent>>
+
+) {
+    for (tumbler, spring) in &tumbler_spring_pairings.array{
+        let Ok(tumbler_t) = transforms.get(*tumbler) else { continue };
+        let bottom_y = tumbler_t.translation.y + HEIGHT_OF_TUMBLER_SPRITE / 2.0;
+
+        let Ok(mut spring_t) = spring_transforms.get_mut(*spring) else { continue };
+        let gap = TOP_OF_CHAMBER - bottom_y;
+        spring_t.translation.y = bottom_y + gap / 2.0;
+        spring_t.scale.y = gap / HEIGHT_OF_SPRING_SPRITE;
+    }
+}
