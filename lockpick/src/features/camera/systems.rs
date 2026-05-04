af9a2805b887a820a367908eb42a84cbb3208295
Gabriel Allison <gabelot12@gmail.com>
2026-05-04 15:31:39 -0400

    Modified Camera System
    Set background to black, zoomed out the projection to capture the full 8 tumbler lock.

--- lockpick/src/features/camera/systems.rs
+++ lockpick/src/features/camera/systems.rs
@@ -1,4 +1,6 @@
 use bevy::prelude::*;
+use bevy::camera::*;
+use bevy::camera::ScalingMode::WindowSize;
 
 pub fn spawn_camera(
     mut commands: Commands,
@@ -12,6 +14,17 @@ pub fn spawn_camera(
         commands.spawn(
             (
                 Camera2d::default(),
+                Camera{
+                    clear_color: ClearColorConfig::Custom(Color::srgb(0.0, 0.0, 0.0)), //Change the background color per camera
+                    ..default()
+                },
+                Projection::from(OrthographicProjection{
+                    scaling_mode: ScalingMode::FixedVertical {
+                        viewport_height:1080.0,
+                    },
+                    scale: 1.3,
+                    ..OrthographicProjection::default_2d()
+                }),
                 Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
             )
         );
