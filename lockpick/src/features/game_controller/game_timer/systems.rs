ac3bcae9e4b1fb6d43b013714a3c1a34eec57cee
Kira Ash Stephenson <SolaasAshenOnslaught@proton.me>
2026-05-07 19:28:37 -0700

    Time is Done
    Chronologs have the ability to countdown now.

--- lockpick/src/features/game_controller/game_timer/systems.rs
+++ lockpick/src/features/game_controller/game_timer/systems.rs
@@ -28,7 +28,6 @@ pub fn the_timer_ticking(
     if let Some(ticker) = &mut log.ticker_for_thousandths { ticker.tick(delta); }
 
     println!("{}", log.get_number());
-
 }
 
 /// Will loop through queried chronologs to see if they have tickers within them that need to be tick-tocked.
@@ -62,6 +61,7 @@ pub fn ticker_ticking(
 
     for mut ticker in &mut tickers {
         ticker.tick(delta);
+
         println!("{}", ticker.get_number());
     }
 }
